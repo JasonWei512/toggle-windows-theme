4c2264894a92caf32279eeadc73d60089d165fbd
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2023-01-18 11:42:45 +0800

    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use windows::w;
 use windows::Win32::Foundation::HWND;
 use windows::Win32::Foundation::LPARAM;
 use windows::Win32::Foundation::WPARAM;
-use windows::Win32::UI::WindowsAndMessaging::SMTO_BLOCK;
 use windows::Win32::UI::WindowsAndMessaging::SendMessageTimeoutW;
+use windows::Win32::UI::WindowsAndMessaging::SMTO_BLOCK;
 use windows::Win32::UI::WindowsAndMessaging::WM_SETTINGCHANGE;
-use windows::w;
 use winreg::enums::*;
 use winreg::RegKey;
 
@@ -15,11 +15,12 @@ const PERSONALIZE_REGISTRY_KEY: &str =
 const APPS_USE_LIGHT_THEME_REGISTRY_KEY: &str = "AppsUseLightTheme";
 const SYSTEM_USES_LIGHT_THEME_REGISTRY_KEY: &str = "SystemUsesLightTheme";
 
-const SYSTEM_REQUIREMENTS_ERROR_MESSAGE: &str = "This program requires Windows 14393 or above";
+const SYSTEM_REQUIREMENTS_ERROR_MESSAGE: &str = "This program requires Windows 10 14393 or above";
 
+#[derive(Debug)]
 enum Theme {
     Light,
-    Dark
+    Dark,
 }
 
 fn main() {
@@ -27,17 +28,19 @@ fn main() {
         panic!("{SYSTEM_REQUIREMENTS_ERROR_MESSAGE}");
     }
 
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let personalize_reg_key = hkcu
+    let personalize_reg_key = RegKey::predef(HKEY_CURRENT_USER)
         .open_subkey_with_flags(PERSONALIZE_REGISTRY_KEY, KEY_READ | KEY_WRITE)
         .expect(SYSTEM_REQUIREMENTS_ERROR_MESSAGE);
 
-    let windows_theme = get_windows_theme(&personalize_reg_key);
+    let current_windows_theme = get_windows_theme(&personalize_reg_key);
+    println!("Current Windows theme is: {:?}", current_windows_theme);
 
-    match windows_theme {
-        Theme::Light => set_windows_theme(Theme::Dark, &personalize_reg_key),
-        Theme::Dark => set_windows_theme(Theme::Light, &personalize_reg_key)
-    }
+    let theme_to_switch_to = match current_windows_theme {
+        Theme::Light => Theme::Dark,
+        Theme::Dark => Theme::Light,
+    };
+    println!("Setting Windows theme to: {:?}", theme_to_switch_to);
+    set_windows_theme(theme_to_switch_to, &personalize_reg_key);
 }
 
 fn get_windows_theme(personalize_reg_key: &RegKey) -> Theme {
@@ -47,21 +50,24 @@ fn get_windows_theme(personalize_reg_key: &RegKey) -> Theme {
 
     match theme_reg_value {
         0 => Theme::Dark,
-        _ => Theme::Light
+        _ => Theme::Light,
     }
 }
 
 fn set_windows_theme(theme: Theme, personalize_reg_key: &RegKey) {
     let reg_value: u32 = match theme {
         Theme::Light => 1,
-        Theme::Dark => 0
+        Theme::Dark => 0,
     };
 
     personalize_reg_key
         .set_value(APPS_USE_LIGHT_THEME_REGISTRY_KEY, &reg_value)
         .expect(SYSTEM_REQUIREMENTS_ERROR_MESSAGE);
 
-    if personalize_reg_key.get_value::<u32, _>(SYSTEM_USES_LIGHT_THEME_REGISTRY_KEY).is_ok() {
+    if personalize_reg_key
+        .get_value::<u32, _>(SYSTEM_USES_LIGHT_THEME_REGISTRY_KEY)
+        .is_ok()
+    {
         let _ = personalize_reg_key.set_value(SYSTEM_USES_LIGHT_THEME_REGISTRY_KEY, &reg_value);
     }
 
@@ -70,15 +76,14 @@ fn set_windows_theme(theme: Theme, personalize_reg_key: &RegKey) {
 
 fn broadcast_windows_theme_changed_message() {
     unsafe {
-        let result = SendMessageTimeoutW(
-            HWND(0xffff),   // HWND_BROADCAST
-            WM_SETTINGCHANGE, 
-            WPARAM::default(), 
+        SendMessageTimeoutW(
+            HWND(0xffff), // HWND_BROADCAST
+            WM_SETTINGCHANGE,
+            WPARAM::default(),
             LPARAM(w!("ImmersiveColorSet").as_ptr() as isize),
-            SMTO_BLOCK, 
-            100, 
-            None);
-        
-        println!("{}", result.0);
+            SMTO_BLOCK,
+            100,
+            None,
+        );
     }
 }
