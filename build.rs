7199f12e5c8a01d0e5b44f07feecb5797e6f93e5
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2023-01-18 10:33:45 +0800

    Code cleanup

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 use winres::WindowsResource;
 
 fn main() {
+    if cfg!(not(windows)) {
+        panic!("This program only supports Windows.");
+    }
+
     WindowsResource::new()
         .set_icon("icon.ico")
         .compile()
